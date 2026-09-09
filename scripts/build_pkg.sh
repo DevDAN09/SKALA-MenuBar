03a4ca716e95e25d275cf9bc4a8920c9e04dab09
윤영민 <devyun09@gmail.com>
2026-09-09 16:15:33 +0900

    chore: bump version to 1.2.1 for release package

--- scripts/build_pkg.sh
+++ scripts/build_pkg.sh
@@ -5,7 +5,7 @@ PROJECT_ROOT="$(cd "$(dirname "$0")/.." && pwd)"
 cd "$PROJECT_ROOT"
 
 APP_NAME="SKALA-MenuBar"
-VERSION="1.2.0"
+VERSION="1.2.1"
 BUNDLE_ID="com.skala.menubar"
 DIST_DIR="$PROJECT_ROOT/dist"
 ROOT_DIR="$DIST_DIR/root"
@@ -56,7 +56,7 @@ cat << PLIST > "$APP_BUNDLE/Contents/Info.plist"
     <key>CFBundleShortVersionString</key>
     <string>$VERSION</string>
     <key>CFBundleVersion</key>
-    <string>5</string>
+    <string>6</string>
     <key>LSMinimumSystemVersion</key>
     <string>13.0</string>
     <key>LSUIElement</key>
